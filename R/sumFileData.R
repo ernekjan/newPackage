686027703b5ccb2071a4d8da2304e5103e426571
ernekjan <ernekjan@fit.cvut.cz>
2015-04-23 10:13:52 +0200

    New function added

---
+++ R/sumFileData.R
@@ -0,0 +1,9 @@
+sumFileData <- function(path, var){
+	file <- read.csv(path, header = TRUE, sep=",", quote="", dec=".")$var
+	vec <- as.numeric(file)
+	suma <- sum(vec)
+	countOfValues <- length(vec)
+	sampleVariance <- var(vec)
+	standardDeviation <- sd(vec)
+	return(cat(sprintf("Count of non-empty values: %d\n Sum: %.3f\n Sample variance: %.3f\n Sample standard deviation: %.3f\n", countOfValues, suma, sampleVariance, standardDeviation))
+}
