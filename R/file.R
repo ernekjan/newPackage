f2433d1c2206418ba70f462b3824a8ca5ab77728
ernekjan <ernekjan@fit.cvut.cz>
2015-04-23 15:51:13 +0200

    Still trying

--- R/file.R
+++ R/file.R
@@ -1,6 +1,4 @@
 file <- function(path){
 	data <- read.csv(path, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
-	vec <- as.numeric(file[,1])
-	sum(vec)
-	summary(data)
+	as.numeric(file[,1])
 }
