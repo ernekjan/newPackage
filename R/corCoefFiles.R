c1b18a03ec3a647b1c5d78ed2d9b2764ad9bef0b
ernekjan <ernekjan@fit.cvut.cz>
2015-04-24 20:40:29 +0200

    Condition added

--- R/corCoefFiles.R
+++ R/corCoefFiles.R
@@ -3,7 +3,11 @@ corCoefFiles <- function(firstPath, secondPath){
 	secondFile <- read.csv(secondPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
 	dataFirstVar <- as.numeric(firstFile[,1])
 	dataSecondVar <- as.numeric(secondFile[,1])
-	corCoef <- cor(dataFirstVar,dataSecondVar)
-	covar <- cov(dataFirstVar,dataSecondVar)
-	cat('Correlation coefficient: ',corCoef,' Covariance: ',covar)
+	if(length(dataFirstVar)==length(dataSecondVar)){
+		corCoef <- cor(dataFirstVar,dataSecondVar)
+		covar <- cov(dataFirstVar,dataSecondVar)
+		cat('Correlation coefficient: ',corCoef,' Covariance: ',covar)
+	}else{
+		cat('Different count of values')
+	}
 }
