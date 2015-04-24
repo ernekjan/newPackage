b2a78ab61892ba93ffab65a18249c9c0064f4959
ernekjan <ernekjan@fit.cvut.cz>
2015-04-24 18:23:25 +0200

    Another function added

---
+++ R/corCoefFiles.R
@@ -0,0 +1,9 @@
+corCoefFiles <- function(firstPath, secondPath){
+	firstFile <- read.csv(firstPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
+	secondFile <- read.csv(secondPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
+	dataFirstVar <- as.numeric(firstFile[,1])
+	dataSecondVar <- as.numeric(secondFile[,1])
+	corCoef <- cor(dataFirstVar,dataSecondVar)
+	covar <- cov(dataFirstVar,dataSecondVar)
+	cat('Correlation coefficient: ',corCoef,' Covariance: ',covar)
+}
