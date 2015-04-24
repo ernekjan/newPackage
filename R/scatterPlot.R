82c8b580b623fc929df05ac2b4e40ddb031f812d
ernekjan <ernekjan@fit.cvut.cz>
2015-04-24 21:06:42 +0200

    Another function added

---
+++ R/scatterPlot.R
@@ -0,0 +1,12 @@
+scatterPlot <- function(firstPath, secondPath){
+	firstFile <- read.csv(firstPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
+	secondFile <- read.csv(secondPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
+	dataFirstVar <- as.numeric(firstFile[,1])
+	dataSecondVar <- as.numeric(secondFile[,1])
+	if(length(dataFirstVar)==length(dataSecondVar)){
+		plot(dataFirstVar, dataSecondVar, xlab="firstVariable", ylab="secondVariable")
+		abline(lm(dataFirstVar ~ dataSecondVar))
+	}else{
+		cat('Different count of values')
+	}
+}
