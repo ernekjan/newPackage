e7e134ce13763198b755d7a3e63c3a3b9e6f6f13
ernekjan <ernekjan@fit.cvut.cz>
2015-05-04 17:31:59 +0200

    edfFile.R changed

--- R/edfFile.R
+++ R/edfFile.R
@@ -2,5 +2,5 @@ edfFile <- function(path){
 	file <- read.csv(path, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
 	data <- as.numeric(file[,1])
 	edf <- ecdf(data)
-	plot(edf)
+	plot(edf, do.points=FALSE)
 }
