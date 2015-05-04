0ff2da8dee3716fbe8a62bfabd6d2a1723171973
ernekjan <ernekjan@fit.cvut.cz>
2015-05-04 16:59:04 +0200

    drawHist.R changed

--- R/drawHist.R
+++ R/drawHist.R
@@ -1,3 +1,3 @@
 drawHist <- function(vec, widthOfBin){
-	hist(vec, right=FALSE, col="yellow", xlab="Value", breaks=seq(min(vec),max(vec), by=widthOfBin) 
+	hist(vec, right=FALSE, col="yellow", xlab="Value", breaks=seq(min(vec),max(vec), by=widthOfBin))
 }
