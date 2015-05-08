c8bc194f53a95ac2432930c89a0a705a3bcf79eb
ernekjan <ernekjan@fit.cvut.cz>
2015-05-08 12:41:19 +0200

    drawHist function changed

--- R/drawHist.R
+++ R/drawHist.R
@@ -1,3 +1,14 @@
-drawHist <- function(vec, widthOfBin){
-	hist(vec, right=FALSE, col="yellow", xlab="Hodnota", ylab="Frekvence", main = paste("Histogram"), breaks=seq(min(vec)-widthOfBin,max(vec)+widthOfBin, by=widthOfBin))
+drawHist <- function(vec, widthOfBin, distribution){
+	hist(vec, right=FALSE, col="yellow", xlab="Hodnota", ylab="Pravděpodobnost", main = paste("Histogram"), breaks=seq(min(vec)-widthOfBin,max(vec)+widthOfBin, by=widthOfBin), prob=TRUE)
+	if(distribution=="normal"){
+		nMean <- mean(vec)
+		lVec <- length(vec)
+		nSd <- sqrt(((lVec-1)/lVec)*var(vec))
+		lines(dnorm(vec, mean=nMean, sd=nSd), col="darkblue")
+	}else if(distribution=="uniform"){
+		lines(dunif(vec, min=min(vec), max=max(vec), log = FALSE), col="red")
+	}else if(distribution=="exponencial"){
+		lambda <- (1/mean(vec))
+		lines(dexp(vec, lambda), col="red")
+	}
 }
