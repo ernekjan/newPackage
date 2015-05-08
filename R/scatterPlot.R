ced11887fe75e18eb126a9d81a2e04044e3af479
ernekjan <ernekjan@fit.cvut.cz>
2015-05-08 17:10:18 +0200

    Another function changed

--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -2,8 +2,7 @@ scatterPlot <- function(firstData, secondData){
 	if(length(firstData)==length(secondData)){
 		if(secondData=="EMPTY"){
 			plot(firstData, firstData, xlab="Data prvního sloupce", ylab="Data druhého sloupce")
-		}else
-		{
+		}else{
 			plot(firstData, secondData, xlab="Data prvního sloupce", ylab="Data druhého sloupce")
 		}
 	}else{
