76d5ba125b12e3d48be1b0a3a5beddce16472675
ernekjan <ernekjan@fit.cvut.cz>
2015-05-08 17:00:14 +0200

    Another function changed

--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -1,4 +1,5 @@
 scatterPlot <- function(firstData, secondData){
+	if(secondData=="EMPTY"){ secondData<-firstData}
 	if(length(firstData)==length(secondData)){
 		plot(firstData, secondData, xlab="První náhodná veličina", ylab="Druhá náhodná veličina")
 	}else{
