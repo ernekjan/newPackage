07fde670828b521d97a8a10fafafd63ac75ccef0
ernekjan <ernekjan@fit.cvut.cz>
2015-05-08 17:02:43 +0200

    Another function changed

--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -1,7 +1,10 @@
-scatterPlot <- function(firstData, secondData){
-	if(secondData=="EMPTY"){ secondData<-firstData}
-	if(length(firstData)==length(secondData)){
-		plot(firstData, secondData, xlab="První náhodná veličina", ylab="Druhá náhodná veličina")
+scatterPlot <- function(firstPath, secondPath){
+	firstFile <- read.csv(firstPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
+	secondFile <- read.csv(secondPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
+	dataFirstVar <- as.numeric(firstFile[,1])
+	dataSecondVar <- as.numeric(secondFile[,1])
+	if(length(dataFirstVar)==length(dataSecondVar)){
+		plot(dataFirstVar, dataSecondVar, xlab="firstVariable", ylab="secondVariable")
 	}else{
 		cat('Different count of values')
 	}
