a0a05a214edf9a8e3a8984adfb66b325b022b28e
ernekjan <ernekjan@fit.cvut.cz>
2015-05-08 15:51:25 +0200

    changed

--- R/twoTailedTTest.R
+++ R/twoTailedTTest.R
@@ -1,3 +1,10 @@
 twoTailedTTest <- function(vec, mu0, alpha){
-	t.test(vec, alternative="two.sided", mu=mu0, paired=FALSE, conf.level=alpha)
+	results=t.test(vec, alternative="two.sided", mu=mu0, paired=FALSE, conf.level=alpha)
+	if(mu0>results$conf.int[1] & mu0<results$conf.int[2]){
+		solution <-TRUE
+	}else{
+		solution <-FALSE
+	}
+	pValue <- results$p.value
+	c(solution, pValue)
 }
