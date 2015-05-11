1f551c994a08312d1d6d60d7ec206e469b04b94c
ernekjan <ernekjan@fit.cvut.cz>
2015-05-11 14:58:04 +0200

    Another functions added

--- R/KSTwoSam.R
+++ R/KSTwoSam.R
@@ -1,4 +1,4 @@
-KSTest <- function(firstPath, secondPath){
+KSTwoSam<- function(firstPath, secondPath){
 	
 	firstFile <- read.csv(firstPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
 	secondFile <- read.csv(secondPath, header = TRUE, sep=",", quote="", dec=".", stringsAsFactors=FALSE)
