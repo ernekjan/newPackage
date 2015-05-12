1c79769c5db877a4763105140e96be89cb51c202
ernekjan <ernekjan@fit.cvut.cz>
2015-05-12 03:09:32 +0200

    Another function changed

--- R/drawHist.R
+++ R/drawHist.R
@@ -1,14 +1,11 @@
-drawHist <- function(vec, widthOfBin, distribution){
+drawHist <- function(vec, widthOfBin){
 	hist(vec, right=FALSE, col="yellow", xlab="Hodnota", ylab="Pravděpodobnost", main = paste("Histogram"), ylim=c(-0,0.11), breaks=seq(min(vec)-widthOfBin,max(vec)+widthOfBin, by=widthOfBin), prob=TRUE)
-	if(distribution=="normal"){
-		nMean <- mean(vec)
-		lVec <- length(vec)
-		nSd <- sqrt(((lVec-1)/lVec)*var(vec))
-		lines(dnorm(vec, mean=nMean, sd=nSd), col="darkblue")
-	}else if(distribution=="uniform"){
-		lines(dunif(vec, min=min(vec), max=max(vec), log = FALSE), col="red")
-	}else if(distribution=="exponencial"){
-		lambda <- (1/mean(vec))
-		lines(dexp(vec, lambda), col="darkgreen")
-	}
+	nMean <- mean(vec)
+	lVec <- length(vec)
+	nSd <- sqrt(((lVec-1)/lVec)*var(vec))
+	lines(dnorm(vec, mean=nMean, sd=nSd), col="darkblue")
+	lines(dunif(vec, min=min(vec), max=max(vec), log = FALSE), col="red")
+	lambda <- (1/mean(vec))
+	lines(dexp(vec, lambda), col="darkgreen")
+	legend ((min(vec)+max(vec))/2, 0.08, c("normalni","rovnomerne","exponencialni"), lty=c(1,1,1), lwd=c(2.5,2.5,2.5), col=c("darkblue","red","darkgreen"))
 }
