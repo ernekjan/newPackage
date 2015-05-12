9af5272d76e4a22c9e4a31f21757a13eeb2e9c18
ernekjan <ernekjan@fit.cvut.cz>
2015-05-12 04:04:58 +0200

    Another function changed

--- R/edfPlot.R
+++ R/edfPlot.R
@@ -1,15 +1,12 @@
-edfPlot <- function(vec, distribution){
+edfPlot <- function(vec){
 	edf <- ecdf(vec)
 	plot(edf,do.points = FALSE, main="Graf empirické distribuční funkce")
-	if(distribution=="normal"){
-		nMean <- mean(vec)
-		lVec <- length(vec)
-		nSd <- sqrt(((lVec-1)/lVec)*var(vec))
-		lines(pnorm(vec, mean=nMean, sd=nSd), col="darkblue")
-	}else if(distribution=="uniform"){
-		lines(punif(vec, min=min(vec), max=max(vec), log = FALSE), col="red")
-	}else if(distribution=="exponencial"){
-		lambda <- (1/mean(vec))
-		lines(pexp(vec, lambda), col="darkgreen")
-	}
+	nMean <- mean(vec)
+	lVec <- length(vec)
+	nSd <- sqrt(((lVec-1)/lVec)*var(vec))
+	lines(pnorm(vec, mean=nMean, sd=nSd), col="darkblue")
+	lines(punif(vec, min=min(vec), max=max(vec), log = FALSE), col="red")
+	lambda <- (1/mean(vec))
+	lines(pexp(vec, lambda), col="darkgreen")
+	legend ((min(vec)+max(vec))/2, 0.1, c("normalni","rovnomerne","exponencialni"), lty=c(1,1,1), lwd=c(2.5,2.5,2.5), col=c("darkblue","red","darkgreen"))
 }
